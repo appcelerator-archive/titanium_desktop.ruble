884ce363ae2a6627fdb2f5721a6b5c7c0d41f7bc
Ingo Muschenetz <ingo@muschenetz.com>
2011-09-23 18:45:35 -0700

    Corrected invalid command definition.

--- bundle.rb
+++ bundle.rb
@@ -6,10 +6,10 @@ bundle 'Appcelerator Titanium Desktop' do |bundle|
   bundle.copyright = "Copyright 2011 Appcelerator. Distributed under the MIT license."
   bundle.contact_email_rot_13 = 'null'
   bundle.description =  'Support for Appcelerator Titanium Desktop'
-  bundle.display_name = 'Titanium Mobile'
+  bundle.display_name = 'Titanium Desktop'
   bundle.repository = "git@github.com:appcelerator/titanium_desktop.ruble.git"
 
   bundle.menu 'Titanium Desktop' do |main_menu|
-    main_menu.command 'Appcelerator Titanium Desktop Developer Center'
+    main_menu.command 'Titanium Desktop Developer Center'
   end
 end
