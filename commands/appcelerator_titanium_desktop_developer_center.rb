1979a6de1f3b9a72fc5eb7a933e0d41e51466391
Ingo Muschenetz <ingo@muschenetz.com>
2011-11-02 10:29:18 -0700

    Updated documentation command to handle the case where there is no selection in the editor. Fixes TISTUD-682.

--- commands/appcelerator_titanium_desktop_developer_center.rb
+++ commands/appcelerator_titanium_desktop_developer_center.rb
@@ -3,11 +3,15 @@ require 'ruble'
 command 'Titanium Desktop Developer Center' do |cmd|
   #cmd.key_binding = 'M4+H'
   cmd.output = :show_as_html
-  cmd.input = :selection 
+  cmd.input = :selection, :word 
   #cmd.input = [:selection, :word]
   cmd.scope = "source.js"
   cmd.invoke do |context|
-    word = $stdin.read
-    "<meta http-equiv='Refresh' content='0;URL=http://developer.appcelerator.com/apidoc/search/desktop/latest?q=#{word}'>"
+    word = $stdin.read  
+    if word.nil? || word == ""
+      url = "<meta http-equiv='Refresh' content='0;URL=http://developer.appcelerator.com/apidoc/desktop/latest'>"    
+    else
+      url = "<meta http-equiv='Refresh' content='0;URL=http://developer.appcelerator.com/apidoc/search/desktop/latest?q=#{word}'>"
+    end
   end
 end
