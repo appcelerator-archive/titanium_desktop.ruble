deb31ee6a4f7f445dba599f93d0d6ec9ccc80009
Ingo Muschenetz <ingo@muschenetz.com>
2012-03-13 12:19:51 -0700

    Added new documentation URLs

--- commands/appcelerator_titanium_desktop_developer_center.rb
+++ commands/appcelerator_titanium_desktop_developer_center.rb
@@ -9,9 +9,9 @@ command 'Titanium Desktop Developer Center' do |cmd|
   cmd.invoke do |context|
     word = $stdin.read  
     if word.nil? || word == ""
-      url = "<meta http-equiv='Refresh' content='0;URL=http://developer.appcelerator.com/apidoc/desktop/latest'>"    
+      url = "<meta http-equiv='Refresh' content='0;URL=http://studio.appcelerator.com/redirect.php?location=docs_desktop_search'>"    
     else
-      url = "<meta http-equiv='Refresh' content='0;URL=http://developer.appcelerator.com/apidoc/search/desktop/latest?q=#{word}'>"
+      url = "<meta http-equiv='Refresh' content='0;URL=http://studio.appcelerator.com/redirect.php?location=docs_desktop_search&q=#{word}'>"
     end
   end
 end
