847a1e2b0b9a6b558e8fdbf353811b4cece91858
Ingo Muschenetz <ingo@muschenetz.com>
2012-03-19 15:03:37 -0700

    Corrected repo URL to use read-only version.

--- bundle.rb
+++ bundle.rb
@@ -5,7 +5,7 @@ bundle 'Appcelerator Titanium Desktop' do |bundle|
   bundle.copyright = "Copyright 2011 Appcelerator. Distributed under the MIT license."
   bundle.description =  'Support for Appcelerator Titanium Desktop'
   bundle.display_name = t(:bundle_name)
-  bundle.repository = "git@github.com:appcelerator/titanium_desktop.ruble.git"
+  bundle.repository = "git://github.com/appcelerator/titanium_desktop.ruble.git"
 
   bundle.menu t(:bundle_name) do |main_menu|
     main_menu.command t(:developer_center)
